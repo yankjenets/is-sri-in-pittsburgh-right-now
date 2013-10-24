3cec8032690c6c1362da7e2a85276a47588acf68
Daniel Deutsch <danieldeutsch@gmail.com>
2013-10-23 20:08:41 -0400

    hardcoded in dummy user foursquare key so you don't have to log in.

--- main.js
+++ main.js
@@ -3,26 +3,28 @@ $(document).ready(function() {
 });
 
 function addSriClick() {
-  $("#sri").click(function() {
-    var accessToken = window.location.hash.split("=")[1];
-    var sriUser = "https://api.foursquare.com/v2/users/61236?oauth_token=" + accessToken + "&v=20131023";
-    $.get(sriUser, function(data, status) {
-      var item = data.response.user.checkins.items[0]
-      var date = item.createdAt;
-      var currentTime = new Date();
-      var minutesSinceCheckin = (currentTime.getTime() / 1000 - date) / 60;
-      var currentLat = item.venue.location.lat;
-      var currentLong = item.venue.location.lng;
-      var kilometersFromPittsburgh = Math.round(calculateDistance(currentLong, currentLat) * 10) / 10;
-      var resultString = "";
-      if (item.venue.location.city != "Pittsburgh") {
-        resultString += "Sri is " + kilometersFromPittsburgh + " kilometers away from Pittsburgh in the city " + item.venue.location.city;
-      } else {
-        resultString += "Sri is in Pittsburgh!";
-      }
-      resultString += "(as recently as " + Math.round(minutesSinceCheckin) + " minutes ago at " + (new Date(date * 1000)).toLocaleTimeString() + ".)";
-      $("#sri").text(resultString);
-    });
+  $("#update").click(update());
+}
+
+function update() {
+  var accessToken = "JGS4NPW5EQUA3OBTI0L5XU33H2UJBHL5XHDUTMRENV2APGHL";
+  var sriUser = "https://api.foursquare.com/v2/users/61236?oauth_token=" + accessToken + "&v=20131023";
+  $.get(sriUser, function(data, status) {
+    var item = data.response.user.checkins.items[0]
+    var date = item.createdAt;
+    var currentTime = new Date();
+    var minutesSinceCheckin = (currentTime.getTime() / 1000 - date) / 60;
+    var currentLat = item.venue.location.lat;
+    var currentLong = item.venue.location.lng;
+    var kilometersFromPittsburgh = Math.round(calculateDistance(currentLong, currentLat) * 10) / 10;
+    var resultString = "";
+    if (item.venue.location.city != "Pittsburgh") {
+      resultString += "Sri is " + kilometersFromPittsburgh + " kilometers away from Pittsburgh in the city " + item.venue.location.city;
+    } else {
+      resultString += "Sri is in Pittsburgh!";
+    }
+    resultString += " (as recently as " + Math.round(minutesSinceCheckin) + " minutes ago at " + (new Date(date * 1000)).toLocaleTimeString() + ".)";
+    $("#sri").text(resultString);
   });
 }
 
